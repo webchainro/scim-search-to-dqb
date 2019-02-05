9b37b17677d9c45dcae987b49d49244a79419299
Andrei Gorgan <andrei.gorgan@webchain.ro>
2019-02-05 22:26:28 +0200

    Fixed tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -22,7 +22,7 @@ class ParserTest extends TestCase
     {
         $parser = new Parser($this->getEntityManager(), User::class);
         $qb = $parser->fromScimToQueryBuilder($filterString);
-        $this->assertEquals(trim(preg_replace('/\s\s+/', ' ', $expectedDql)), $qb->getQuery()->getDQL());
+        $this->assertEquals($expectedDql, $qb->getQuery()->getDQL());
         $parametersArray = $qb->getParameters()->toArray();
         $resultParametersArray = [];
         /** @var Parameter $parameter */
@@ -37,183 +37,71 @@ class ParserTest extends TestCase
         return [
             [
                 'userName eq "bjensen"',
-                "
-                 SELECT 
-                    sftdp 
-                 FROM 
-                    " . User::class . " sftdp 
-                 WHERE 
-                    sftdp.userName = ?1",
-
+                "SELECT sftdp FROM " . User::class . " sftdp WHERE sftdp.userName = ?1",
                 [1 => "bjensen"]
             ],
 
             [
                 'name.familyName co "O\'Malley"',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.name sftdj1 
-                WHERE 
-                    sftdj1.familyName LIKE '%O''Malley%'",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.name sftdj1 WHERE sftdj1.familyName LIKE '%O''Malley%'",
                 []
             ],
 
             [
                 'userName sw "J"',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                WHERE 
-                    sftdp.userName LIKE 'J%'",
+                "SELECT sftdp FROM " . User::class . " sftdp WHERE sftdp.userName LIKE 'J%'",
                 []
             ],
             [
                 'title pr',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                WHERE 
-                    sftdp.title IS NOT NULL",
+                "SELECT sftdp FROM " . User::class . " sftdp WHERE sftdp.title IS NOT NULL",
                 []
             ],
 
             [
                 'meta.lastModified gt "2019-01-28T04:42:34Z"',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.meta sftdj1 
-                WHERE 
-                    sftdj1.lastModified > ?1",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.meta sftdj1 WHERE sftdj1.lastModified > ?1",
                 [1 => new \DateTime('2019-01-28T04:42:34Z')]
             ],
             [
                 'title pr and userType eq "Employee"',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                WHERE 
-                    sftdp.title IS NOT NULL 
-                AND 
-                    sftdp.userType = ?1",
+                "SELECT sftdp FROM " . User::class . " sftdp WHERE sftdp.title IS NOT NULL AND sftdp.userType = ?1",
                 [1 => 'Employee']
             ],
 
             [
                 'title pr or userType eq "Intern"',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                WHERE 
-                    sftdp.title IS NOT NULL 
-                OR 
-                    sftdp.userType = ?1",
+                "SELECT sftdp FROM " . User::class . " sftdp WHERE sftdp.title IS NOT NULL OR sftdp.userType = ?1",
                 [1 => 'Intern']
             ],
             [
                 'userType eq "Employee" and (emails co "example.com" or emails.value co "example.org")',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                WHERE 
-                    sftdp.userType = ?1 
-                AND (
-                    sftdj1.value LIKE '%example.com%' OR sftdj1.value LIKE '%example.org%')",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 WHERE sftdp.userType = ?1 AND (sftdj1.value LIKE '%example.com%' OR sftdj1.value LIKE '%example.org%')",
                 [1 => 'Employee']
             ],
             [
                 'userType ne "Employee" and not (emails co "example.com" or emails.value co "example.org")',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                WHERE 
-                    sftdp.userType <> ?1 
-                AND (
-                    sftdj1.value NOT LIKE '%example.com%' 
-                    AND 
-                    sftdj1.value NOT LIKE '%example.org%'
-                )",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 WHERE sftdp.userType <> ?1 AND (sftdj1.value NOT LIKE '%example.com%' AND sftdj1.value NOT LIKE '%example.org%')",
                 [1 => 'Employee']
             ],
             [
                 'userType eq "Employee" and (emails.type eq "work")',
-                "SELECT 
-                    sftdp 
-                FROM 
-                    " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                WHERE 
-                    sftdp.userType = ?1 
-                AND 
-                    sftdj1.type = ?2",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 WHERE sftdp.userType = ?1 AND sftdj1.type = ?2",
                 [1 => 'Employee', 2 => 'work']
             ],
             [
                 'emails[type eq "work"]',
-                "SELECT 
-                  sftdp 
-                FROM 
-                  " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                WHERE 
-                    sftdj1.type = ?1",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 WHERE sftdj1.type = ?1",
                 [1 => 'work']
             ],
             [
                 'userType eq "Employee" and emails[type eq "work" and value co "@example.com"]',
-                "SELECT 
-                  sftdp 
-                FROM 
-                  " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                WHERE 
-                    sftdp.userType = ?1 
-                AND (
-                    sftdj1.type = ?2 
-                    AND 
-                    sftdj1.value LIKE '%@example.com%'
-                )",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 WHERE sftdp.userType = ?1 AND (sftdj1.type = ?2 AND sftdj1.value LIKE '%@example.com%')",
                 [1 => 'Employee', 2 => "work"]
             ],
             [
                 'emails[type eq "work" and value co "@example.com"] or ims[type eq "xmpp" and value co "@foo.com"]',
-                "SELECT 
-                  sftdp 
-                FROM 
-                  " . User::class . " sftdp 
-                LEFT JOIN 
-                    sftdp.emails sftdj1 
-                LEFT JOIN 
-                    sftdp.ims sftdj2 
-                WHERE (
-                    sftdj1.type = ?1 
-                    AND 
-                    sftdj1.value LIKE '%@example.com%'
-                ) 
-                OR 
-                (
-                    sftdj2.type = ?2 
-                    AND 
-                    sftdj2.value LIKE '%@foo.com%'
-                )",
+                "SELECT sftdp FROM " . User::class . " sftdp LEFT JOIN sftdp.emails sftdj1 LEFT JOIN sftdp.ims sftdj2 WHERE (sftdj1.type = ?1 AND sftdj1.value LIKE '%@example.com%') OR (sftdj2.type = ?2 AND sftdj2.value LIKE '%@foo.com%')",
                 [1 => 'work', 2 => 'xmpp']
             ],
         ];
